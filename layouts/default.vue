8e63c1c6d83bf28778e984396e2645d51d6ce195
Kyle Plaugher <kaplaugher94@gmail.com>
2018-05-08 17:29:44 -0400

    init

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <section class="hero is-primary has-text-centered">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        <nuxt-link to="/">Nuxt Blog</nuxt-link>
+      </h1>
+      <h2 class="subtitle">
+        Hero subtitle
+      </h2>
+    </div>
+  </div>
+  </section>
+  <nuxt/>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
